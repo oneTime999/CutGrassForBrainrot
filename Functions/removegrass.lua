8a4dd70ee77ea522029f5dd45f8ff9854adf61ad
oneTime999 <onetime.zkk@gmail.com>
2026-03-11 09:50:30 -0300

    Update removegrass.lua

--- Functions/removegrass.lua
+++ Functions/removegrass.lua
@@ -3,11 +3,19 @@ return function(Parent)
         Title = "Remove Grass",
         Desc = "Removes all GrassLine objects from the workspace.",
         Callback = function()
-            for _, obj in ipairs(workspace:GetDescendants()) do
-                if obj.Name == "GrassLine" then
-                    obj:Destroy()
+            local function removeGrassLine(parent)
+                for _, child in ipairs(parent:GetChildren()) do
+                    if not child:IsA("Terrain") and not child:IsA("Camera") then
+                        if #child:GetChildren() > 0 then
+                            removeGrassLine(child)
+                        end
+                    end
+                    if child.Name == "GrassLine" then
+                        child:Destroy()
+                    end
                 end
             end
+            removeGrassLine(workspace)
         end,
     })
 end
