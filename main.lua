20027643472a573b760da695ae1e26d650fe0611
oneTime999 <onetime.zkk@gmail.com>
2026-03-11 09:35:49 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,16 +6,15 @@ local WindUI = loadstring(game:HttpGet(
 ))()
 
 local Window = WindUI:CreateWindow({
-    Title        = "Slow Hub",
-    Icon         = "zap",
-    Author       = "Slow Hub Team",
-    Folder       = "SlowHub",
-    Size         = UDim2.fromOffset(580, 460),
-    Transparent  = true,
-    Theme        = "Dark",
+    Title = "Slow Hub",
+    Icon = "zap",
+    Author = "Slow Hub Team",
+    Folder = "SlowHub",
+    Size = UDim2.fromOffset(580, 460),
+    Transparent = true,
+    Theme = "Dark",
     SideBarWidth = 200,
 })
-
 
 local Tabs = {
     "main",
