6d200e3ff043c7b46791027b82d767eb6aba943e
oneTime999 <onetime.zkk@gmail.com>
2026-03-11 09:23:13 -0300

    Update main.lua

--- Tabs/main.lua
+++ Tabs/main.lua
@@ -1,1 +1,18 @@
+local RepoURL = "https://raw.githubusercontent.com/oneTime999/CutGrassForBrainrot/main/"
 
+return function(Window)
+    local Tab = Window:Tab({
+        Name = "Main",
+        Icon = "house",
+    })
+
+    Tab:Section({ Name = "Island Teleports" })
+
+    Tab:Paragraph({
+        Title   = "How to use",
+        Content = "Select the desired island from the dropdown below, then click the Teleport button to teleport to the selected zone.",
+    })
+
+    local TeleportZones = loadstring(game:HttpGet(RepoURL .. "Functions/teleportzones.lua", true))()
+    TeleportZones(Tab)
+end
